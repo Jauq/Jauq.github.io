51d838f364926040e1d9158102b56f5692cf6190
Tim Anfilofieff <39632133+Jauq@users.noreply.github.com>
2019-11-26 08:36:13 -0800

    Undelete
    I did not intend to delete these files.

---
+++ views/footer.js
@@ -0,0 +1,43 @@
+document.write('\
+\
+  <!-- Contact -->\
+  <div class="container-fluid" id="contact">\
+    <div class="row justify-content-center">\
+        <div class="col-4-auto text-center boxStyle1">\
+            Find Me Elsewhere\
+        </div>\
+    </div>\
+\
+    <div class="row justify-content-center linkBox">\
+        <div class="col text-center justify-content-center">\
+            <div class="linkIcon">\
+                <a class="btn btn-dark customLinkButton clbGithub" href="https://github.com/Jauq" target="_blank" role="button"></a>\
+            </div>\
+            <div class="linkIcon">Github</div>\
+        </div>\
+        <div class="col text-center justify-content-center">\
+            <div class="linkIcon">\
+                <a class="btn btn-dark customLinkButton clbLinked" href="https://www.linkedin.com/in/tim-anfilofieff-799645170/" target="_blank" role="button"></a>\
+            </div>\
+            <div class="linkIcon">LinkedIn</div>\
+        </div>\
+        <div class="col text-center justify-content-center">\
+            <div class="linkIcon">\
+                <a class="btn btn-dark customLinkButton clbNewG" href="https://jauqar.newgrounds.com/" target="_blank" role="button"></a>\
+            </div>\
+            <div class="linkIcon">Newgrounds</div>\
+        </div>\
+    </div>\
+  </div>\
+  <!-- /Contact -->\
+\
+  <!-- Footer -->\
+  <div class="jumbotron-fluid">\
+    <div class="customFooter">\
+        <p class="jumboTextSmall">&copy; Copyright 2019, Tim Anfilofieff</p>\
+        <!-- Footer Image: https://pixabay.com/en/halftone-vector-background-square-2492079/ -->\
+    </div>\
+  </div>\
+  <!-- /Footer -->\
+\
+'); 
