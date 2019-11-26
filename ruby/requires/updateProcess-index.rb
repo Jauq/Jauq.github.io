814d6caadbcaca5b3e3252c6bfd95ef46dab0057
Tim Anfilofieff <39632133+Jauq@users.noreply.github.com>
2019-11-26 10:25:35 -0800

    Ruby Tool Restructure
    Moved my ruby tools around within the Ruby file and restructured some of them to allow for future use in other senses. Also so grammar and image alterations.

---
+++ ruby/requires/updateProcess-index.rb
@@ -0,0 +1,32 @@
+def updateIndex(name = "index.html")
+
+  v = getFiles("../content/home")
+  v = v.reverse
+
+  $f = []
+
+  $f.push("<!doctype html>")
+  $f.push("<html lang=\"en\">"); hn
+  $f.push("   <script src=\"presets/head.js\"></script>"); hn
+  $f.push("   <body>"); hn
+  $f.push("     <script src=\"presets/jumbo.js\"></script>"); hn
+  $f.push("     <script src=\"presets/navbar.js\"></script>"); hn
+  $f.push("     <div class=\"afterScroll\">"); hn
+  cm("Main Section")
+  $f.push("       <div class=\"container\">"); hn
+  v.each do |contents|
+    genContent($f, contents); hn
+  end
+  $f.push("       </div>")
+  cm("Main Section", true); hn
+  $f.push("       <script src=\"presets/footer.js\"></script>"); hn
+  $f.push("     </div>"); hn
+  $f.push("     <script src=\"scripts/navbarSticky.js\"></script>"); hn
+  $f.push("     <script src=\"scripts/footerYear.js\"></script>"); hn
+  $f.push("   </body>")
+  $f.push("</html>")
+
+  File.new("../#{name}", "w+")
+  File.write("../#{name}", $f.join("\n"), mode: "a")
+
+end
---
+++ ruby/updateAction-newindex.rb
@@ -0,0 +1,8 @@
+require_relative "getFiles.rb"
+requireRubyFiles("requires")
+
+puts "Updating newIndex.html..."
+
+updateIndex("newIndex.html")
+
+endScript
