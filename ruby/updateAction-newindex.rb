e08cbefa410301020f44239edc80f210efe06ce0
Tim Anfilofieff <39632133+Jauq@users.noreply.github.com>
2019-11-26 14:49:09 -0800

    New Bord and Combined Update Tools.
    I got rid of that ugly bird picture and replaced with a slightly less ugly one. I also combined both of the update tools I had into one singular tool.

--- ruby/updateAction-newindex.rb
+++
@@ -1,8 +0,0 @@
-require_relative "getFiles.rb"
-requireRubyFiles("requires")
-
-puts "Updating newIndex.html..."
-
-updateIndex("newIndex.html")
-
-endScript
---
+++ ruby/updates.rb
@@ -0,0 +1,45 @@
+require_relative "getFiles.rb"
+requireRubyFiles("requires")
+
+puts "Updating Script. What would you like to update?\n"
+puts "[1] newindex"
+puts "[2] presets"
+puts "[3] all updateable files"
+
+print "\nType the id of what you want to update:  "
+input = gets.chomp.to_i
+
+if input == 1
+  puts "Updating newIndex.html..."
+
+  updateIndex("newIndex.html")
+
+  endScript
+elsif input == 2
+  puts "Updating all presets..."
+
+  files = getFiles("../presets", "html")
+
+  files.each do |f|
+    htmlPresetMaker(f[0...f.length-5])
+  end
+
+  endScript
+elsif input == 3
+  puts "Updating newIndex.html..."
+
+  updateIndex("newIndex.html")
+  
+  puts "Updating all presets..."
+
+  files = getFiles("../presets", "html")
+
+  files.each do |f|
+    htmlPresetMaker(f[0...f.length-5])
+  end
+
+endScript
+else
+  puts "Unknown thing to update."
+  gets
+end
