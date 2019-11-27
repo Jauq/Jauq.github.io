117ea54b6c5e363558595e688ac63c0bbdf984b6
Tim Anfilofieff <39632133+Jauq@users.noreply.github.com>
2019-11-27 08:34:15 -0800

    Moved index into a folder
    Let's see what happens Now that it is no longer in the root directory.

--- ruby/updates.rb
+++ ruby/updates.rb
@@ -8,30 +8,24 @@ puts "[3] presets"
 
 print "\nType the id of what you want to update:  "
 input = gets.chomp.to_i
-
+update = true
 if input == 1
   puts "Updating newIndex.html..."
-
   updateIndex("newIndex.html")
-
-  endScript
 elsif input == 2
   puts "Updating games.html..."
-
   updateGames
-
-  endScript
 elsif input == 3
   puts "Updating all presets..."
-
   files = getFiles("../presets", "html")
-
   files.each do |f|
     htmlPresetMaker(f[0...f.length-5])
   end
-
-  endScript
 else
   puts "Unknown thing to update."
+  update = false
   gets
+end
+if update
+  endScript
 end
