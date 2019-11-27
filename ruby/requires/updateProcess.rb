e94d429e9cc84276671c10e022cb52fde5fdb435
Tim Anfilofieff <39632133+Jauq@users.noreply.github.com>
2019-11-27 08:51:00 -0800

    Other Pages and Merging
    Wow I was able to generate those other pages quickly.
    I also combined all the updateProcess ruby scripts into a single ruby file.

---
+++ ruby/requires/updateProcess.rb
@@ -0,0 +1,151 @@
+def htmlPresetMaker(filePath)
+
+  fileData = File.read("#{filePath}.html").split("\n")
+
+  newData = []
+
+  fileData.count.times do |z|
+      newData.push("#{fileData[z]}\\")
+  end
+
+  newData.insert(0, "document.write('\\")
+
+  newData.push("');")
+
+  File.new("#{filePath}.js", "w+")
+  File.write("#{filePath}.js", newData.join("\n"), mode: "a")
+
+end
+
+def updateIndex(filePath = "index.html")
+
+  files = getFiles("../content/home")
+  files = files.reverse
+
+  $f = []
+
+  $f.push("<!doctype html>")
+  $f.push("<html lang=\"en\">"); hn
+  $f.push("   <script src=\"presets/head.js\"></script>"); hn
+  $f.push("   <body>"); hn
+  $f.push("     <script src=\"presets/indexJumbo.js\"></script>"); hn
+  $f.push("     <script src=\"presets/navbar.js\"></script>"); hn
+  $f.push("     <div class=\"afterScroll fluidBlocks\">"); hn
+  cm("Main Section")
+  $f.push("       <div class=\"container\">"); hn
+  files.each do |contents|
+    genContent($f, contents); hn
+  end
+  $f.push("       </div>")
+  cm("Main Section", true); hn
+  $f.push("       <script src=\"presets/footer.js\"></script>"); hn
+  $f.push("     </div>"); hn
+  $f.push("     <script src=\"scripts/navbarSticky.js\"></script>"); hn
+  $f.push("     <script src=\"scripts/footerYear.js\"></script>"); hn
+  $f.push("   </body>")
+  $f.push("</html>")
+
+  File.new("../#{filePath}", "w+")
+  File.write("../#{filePath}", $f.join("\n"), mode: "a")
+
+end
+
+def updateGames(filePath = "views/games.html")
+
+  $f = []
+
+  $f.push("<!doctype html>")
+  $f.push("<html lang=\"en\">"); hn
+  $f.push("   <script src=\"../presets/viewsHead.js\"></script>"); hn
+  $f.push("   <body>"); hn
+  $f.push("     <script src=\"../presets/viewsNavbar.js\"></script>"); hn
+  $f.push("     <div class=\"afterScroll fluidBlocks\">"); hn
+  cm("Main Section")
+
+  cm("Main Section", true); hn
+  $f.push("       <script src=\"../presets/footer.js\"></script>"); hn
+  $f.push("     </div>"); hn
+  $f.push("     <script src=\"../scripts/navbarSticky.js\"></script>"); hn
+  $f.push("     <script src=\"../scripts/footerYear.js\"></script>"); hn
+  $f.push("   </body>")
+  $f.push("</html>")
+
+  File.new("../#{filePath}", "w+")
+  File.write("../#{filePath}", $f.join("\n"), mode: "a")
+
+end
+
+def updateMusic(filePath = "views/music.html")
+
+  $f = []
+
+  $f.push("<!doctype html>")
+  $f.push("<html lang=\"en\">"); hn
+  $f.push("   <script src=\"../presets/viewsHead.js\"></script>"); hn
+  $f.push("   <body>"); hn
+  $f.push("     <script src=\"../presets/viewsNavbar.js\"></script>"); hn
+  $f.push("     <div class=\"afterScroll fluidBlocks\">"); hn
+  cm("Main Section")
+
+  cm("Main Section", true); hn
+  $f.push("       <script src=\"../presets/footer.js\"></script>"); hn
+  $f.push("     </div>"); hn
+  $f.push("     <script src=\"../scripts/navbarSticky.js\"></script>"); hn
+  $f.push("     <script src=\"../scripts/footerYear.js\"></script>"); hn
+  $f.push("   </body>")
+  $f.push("</html>")
+
+  File.new("../#{filePath}", "w+")
+  File.write("../#{filePath}", $f.join("\n"), mode: "a")
+
+end
+
+def updateArt(filePath = "views/art.html")
+
+  $f = []
+
+  $f.push("<!doctype html>")
+  $f.push("<html lang=\"en\">"); hn
+  $f.push("   <script src=\"../presets/viewsHead.js\"></script>"); hn
+  $f.push("   <body>"); hn
+  $f.push("     <script src=\"../presets/viewsNavbar.js\"></script>"); hn
+  $f.push("     <div class=\"afterScroll fluidBlocks\">"); hn
+  cm("Main Section")
+
+  cm("Main Section", true); hn
+  $f.push("       <script src=\"../presets/footer.js\"></script>"); hn
+  $f.push("     </div>"); hn
+  $f.push("     <script src=\"../scripts/navbarSticky.js\"></script>"); hn
+  $f.push("     <script src=\"../scripts/footerYear.js\"></script>"); hn
+  $f.push("   </body>")
+  $f.push("</html>")
+
+  File.new("../#{filePath}", "w+")
+  File.write("../#{filePath}", $f.join("\n"), mode: "a")
+
+end
+
+def updateMisc(filePath = "views/misc.html")
+
+  $f = []
+
+  $f.push("<!doctype html>")
+  $f.push("<html lang=\"en\">"); hn
+  $f.push("   <script src=\"../presets/viewsHead.js\"></script>"); hn
+  $f.push("   <body>"); hn
+  $f.push("     <script src=\"../presets/viewsNavbar.js\"></script>"); hn
+  $f.push("     <div class=\"afterScroll fluidBlocks\">"); hn
+  cm("Main Section")
+
+  cm("Main Section", true); hn
+  $f.push("       <script src=\"../presets/footer.js\"></script>"); hn
+  $f.push("     </div>"); hn
+  $f.push("     <script src=\"../scripts/navbarSticky.js\"></script>"); hn
+  $f.push("     <script src=\"../scripts/footerYear.js\"></script>"); hn
+  $f.push("   </body>")
+  $f.push("</html>")
+
+  File.new("../#{filePath}", "w+")
+  File.write("../#{filePath}", $f.join("\n"), mode: "a")
+
+end
--- ruby/updates.rb
+++ ruby/updates.rb
@@ -4,18 +4,47 @@ requireRubyFiles("requires")
 puts "Updating Script. What would you like to update?\n"
 puts "[1] newindex"
 puts "[2] games"
-puts "[3] presets"
+puts "[3] music"
+puts "[4] art"
+puts "[5] misc"
+puts "[6] presets"
+puts "[7] all"
 
 print "\nType the id of what you want to update:  "
 input = gets.chomp.to_i
 update = true
 if input == 1
-  puts "Updating newIndex.html..."
-  updateIndex("newIndex.html")
+  puts "Updating newindex.html..."
+  updateIndex("newindex.html")
 elsif input == 2
   puts "Updating games.html..."
   updateGames
 elsif input == 3
+  puts "Updating music.html..."
+  updateMusic
+elsif input == 4
+  puts "Updating art.html..."
+  updateArt
+elsif input == 5
+  puts "Updating misc.html..."
+  updateMisc
+elsif input == 6
+  puts "Updating all presets..."
+  files = getFiles("../presets", "html")
+  files.each do |f|
+    htmlPresetMaker(f[0...f.length-5])
+  end
+elsif input == 7
+  puts "Updating newindex.html..."
+  updateIndex("newindex.html")
+  puts "Updating games.html..."
+  updateGames
+  puts "Updating music.html..."
+  updateMusic
+  puts "Updating art.html..."
+  updateArt
+  puts "Updating misc.html..."
+  updateMisc
   puts "Updating all presets..."
   files = getFiles("../presets", "html")
   files.each do |f|
